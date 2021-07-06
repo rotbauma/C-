5ba1f33726e80abb342da39634418d3924c3ec14
rotbauma <86696831+rotbauma@users.noreply.github.com>
2021-07-06 14:06:55 -0700

    Add files via upload

---
+++ Coffee Shop/Form1.Designer.cs
@@ -0,0 +1,384 @@
+﻿
+namespace Coffee_Shop
+{
+    partial class frmTomsDiner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTomsDiner));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.txtDateOfSale = new System.Windows.Forms.TextBox();
+            this.txtNumOfCoffees = new System.Windows.Forms.TextBox();
+            this.txtNumOfDonuts = new System.Windows.Forms.TextBox();
+            this.txtNumOfCookies = new System.Windows.Forms.TextBox();
+            this.txtNumOfMuffins = new System.Windows.Forms.TextBox();
+            this.txtNumOfBagels = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblCostNoSalesTax = new System.Windows.Forms.Label();
+            this.lblSalesTax = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lblSaleDate = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(281, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer\'s &Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(664, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "&Sale Date:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(266, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number of Co&ffee’s :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(598, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Number of &Donut’s :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(257, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(163, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Number of &Cookies’s :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(590, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(158, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Number of &Muffins’s :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(265, 168);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(155, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Number of &Bagels’s :";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(426, 17);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(134, 26);
+            this.txtCustomerName.TabIndex = 7;
+            // 
+            // txtDateOfSale
+            // 
+            this.txtDateOfSale.Location = new System.Drawing.Point(754, 17);
+            this.txtDateOfSale.Name = "txtDateOfSale";
+            this.txtDateOfSale.Size = new System.Drawing.Size(132, 26);
+            this.txtDateOfSale.TabIndex = 8;
+            // 
+            // txtNumOfCoffees
+            // 
+            this.txtNumOfCoffees.Location = new System.Drawing.Point(426, 66);
+            this.txtNumOfCoffees.Name = "txtNumOfCoffees";
+            this.txtNumOfCoffees.Size = new System.Drawing.Size(134, 26);
+            this.txtNumOfCoffees.TabIndex = 9;
+            // 
+            // txtNumOfDonuts
+            // 
+            this.txtNumOfDonuts.Location = new System.Drawing.Point(754, 66);
+            this.txtNumOfDonuts.Name = "txtNumOfDonuts";
+            this.txtNumOfDonuts.Size = new System.Drawing.Size(132, 26);
+            this.txtNumOfDonuts.TabIndex = 10;
+            // 
+            // txtNumOfCookies
+            // 
+            this.txtNumOfCookies.Location = new System.Drawing.Point(426, 116);
+            this.txtNumOfCookies.Name = "txtNumOfCookies";
+            this.txtNumOfCookies.Size = new System.Drawing.Size(134, 26);
+            this.txtNumOfCookies.TabIndex = 11;
+            // 
+            // txtNumOfMuffins
+            // 
+            this.txtNumOfMuffins.Location = new System.Drawing.Point(754, 119);
+            this.txtNumOfMuffins.Name = "txtNumOfMuffins";
+            this.txtNumOfMuffins.Size = new System.Drawing.Size(132, 26);
+            this.txtNumOfMuffins.TabIndex = 12;
+            // 
+            // txtNumOfBagels
+            // 
+            this.txtNumOfBagels.Location = new System.Drawing.Point(426, 165);
+            this.txtNumOfBagels.Name = "txtNumOfBagels";
+            this.txtNumOfBagels.Size = new System.Drawing.Size(134, 26);
+            this.txtNumOfBagels.TabIndex = 13;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(246, 344);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(110, 45);
+            this.btnCalc.TabIndex = 14;
+            this.btnCalc.Text = "C&alculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(426, 344);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(110, 45);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(602, 344);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(110, 45);
+            this.btnExit.TabIndex = 16;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(251, 271);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(50, 20);
+            this.label20.TabIndex = 17;
+            this.label20.Text = "Cost :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(474, 271);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(86, 20);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Sales Tax :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(704, 273);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 20);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Total :";
+            // 
+            // lblCostNoSalesTax
+            // 
+            this.lblCostNoSalesTax.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblCostNoSalesTax.Location = new System.Drawing.Point(307, 268);
+            this.lblCostNoSalesTax.Name = "lblCostNoSalesTax";
+            this.lblCostNoSalesTax.Size = new System.Drawing.Size(100, 23);
+            this.lblCostNoSalesTax.TabIndex = 20;
+            this.lblCostNoSalesTax.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblSalesTax
+            // 
+            this.lblSalesTax.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblSalesTax.Location = new System.Drawing.Point(566, 268);
+            this.lblSalesTax.Name = "lblSalesTax";
+            this.lblSalesTax.Size = new System.Drawing.Size(100, 23);
+            this.lblSalesTax.TabIndex = 21;
+            this.lblSalesTax.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblTotal.Location = new System.Drawing.Point(762, 272);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(100, 23);
+            this.lblTotal.TabIndex = 22;
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(16, 17);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(213, 243);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 24;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(242, 218);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(143, 20);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "Customer\'s Name :";
+            // 
+            // lblCustomerName
+            // 
+            this.lblCustomerName.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblCustomerName.Location = new System.Drawing.Point(391, 215);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Size = new System.Drawing.Size(145, 26);
+            this.lblCustomerName.TabIndex = 26;
+            this.lblCustomerName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(598, 218);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(88, 20);
+            this.label11.TabIndex = 27;
+            this.label11.Text = "Sale Date :";
+            // 
+            // lblSaleDate
+            // 
+            this.lblSaleDate.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblSaleDate.Location = new System.Drawing.Point(692, 215);
+            this.lblSaleDate.Name = "lblSaleDate";
+            this.lblSaleDate.Size = new System.Drawing.Size(145, 26);
+            this.lblSaleDate.TabIndex = 28;
+            this.lblSaleDate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // frmTomsDiner
+            // 
+            this.AcceptButton = this.btnCalc;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 409);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblSaleDate);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtNumOfBagels);
+            this.Controls.Add(this.txtNumOfCookies);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtNumOfMuffins);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtNumOfDonuts);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNumOfCoffees);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDateOfSale);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCustomerName);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblSalesTax);
+            this.Controls.Add(this.lblCostNoSalesTax);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "frmTomsDiner";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tom\'s Diner";
+            this.Load += new System.EventHandler(this.frmTomsDiner_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.TextBox txtDateOfSale;
+        private System.Windows.Forms.TextBox txtNumOfCoffees;
+        private System.Windows.Forms.TextBox txtNumOfDonuts;
+        private System.Windows.Forms.TextBox txtNumOfCookies;
+        private System.Windows.Forms.TextBox txtNumOfMuffins;
+        private System.Windows.Forms.TextBox txtNumOfBagels;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblCostNoSalesTax;
+        private System.Windows.Forms.Label lblSalesTax;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblCustomerName;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lblSaleDate;
+    }
+}
+
---
+++ Coffee Shop/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coffee_Shop
+{
+    public partial class frmTomsDiner : Form
+    {
+        public frmTomsDiner()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Close the program
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // Clear the text boxes
+            txtCustomerName.Text = string.Empty;
+            txtDateOfSale.Text = string.Empty;
+            txtNumOfCoffees.Text = string.Empty;
+            txtNumOfDonuts.Text = string.Empty;
+            txtNumOfCookies.Text = string.Empty;
+            txtNumOfBagels.Text = string.Empty;
+            txtNumOfMuffins.Text = string.Empty;
+
+            //Clear labels
+            lblCustomerName.Text = string.Empty;
+            lblSaleDate.Text = string.Empty;
+            lblCostNoSalesTax.Text = string.Empty;
+            lblSalesTax.Text = string.Empty;
+            lblTotal.Text = string.Empty;
+
+            // Set focus to customer name
+            txtCustomerName.Focus();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            // Declare variables
+            string customerName;
+            string salesDate;
+            int intNumOfCoffees;
+            int intNumOfDonuts;
+            int intNumOfCookies;
+            int intNumOfBagels;
+            int intNumOfMuffins;
+            decimal decCost;
+            decimal decSalesTax;
+            decimal decTotal;
+
+            // Declare constants
+            const decimal SALES_TAX = .06m;
+            const decimal PRICE_COFFEE = 2.55m;
+            const decimal PRICE_DONUT = 1.45M;
+            const decimal PRICE_COOKIE = .70M;
+            const decimal PRICE_MUFFIN = .95M;
+            const decimal PRICE_BAGEL = 1.55M;
+
+            // Convert strings to integers
+            int.TryParse(txtNumOfCoffees.Text, out intNumOfCoffees);
+            int.TryParse(txtNumOfDonuts.Text, out intNumOfDonuts);
+            int.TryParse(txtNumOfCookies.Text, out intNumOfCookies);
+            int.TryParse(txtNumOfBagels.Text, out intNumOfBagels);
+            int.TryParse(txtNumOfMuffins.Text, out intNumOfMuffins);
+
+            customerName = txtCustomerName.Text;
+            salesDate = txtDateOfSale.Text;
+
+            // Math calculations
+            decCost = (PRICE_COFFEE * intNumOfCoffees) + (PRICE_DONUT * intNumOfDonuts) 
+                + (PRICE_COOKIE * intNumOfCookies) + (PRICE_MUFFIN * intNumOfMuffins) 
+                + (PRICE_BAGEL * intNumOfBagels);
+
+            decSalesTax = decCost * SALES_TAX;
+
+            decTotal = decCost + decSalesTax;
+            
+            // Display output
+            lblCustomerName.Text = customerName;
+            lblSaleDate.Text = salesDate;
+            lblCostNoSalesTax.Text = decCost.ToString("#.##");
+            lblSalesTax.Text = decSalesTax.ToString("#.##");
+            lblTotal.Text = decTotal.ToString("#.##");
+
+
+        }
+
+        private void frmTomsDiner_Load(object sender, EventArgs e)
+        {
+            this.Text = "Customer Order - Tom's Diner";
+        }
+    }
+}
